dec52ba805205731873d8ea85bec4b691613a934
rishabhtak <rishabhtak93@gmail.com>
2023-06-14 18:28:13 +0530

    update notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -20,7 +20,7 @@ router.get('/fetchallnotes', fetchuser,
 
    })
 
-//Route 1: add notes using POST '/api/notes/addnotes' Login required
+//Route 2: add notes using POST '/api/notes/addnotes' Login required
 
 router.post('/addnotes', fetchuser, [
    body('title', "Enter a valid title").isLength({ min: 3 }),
@@ -36,6 +36,40 @@ router.post('/addnotes', fetchuser, [
          const notes = new Note({ title, description, tag, user: req.user.id })
          const saveNote = await notes.save();
          res.json({ saveNote })
+      }
+      catch (error) {
+         res.status(500).send("Internal server error");
+      }
+
+   })
+
+
+//Route 3: update notes using PUT '/api/notes/updatenotes' Login required
+
+router.put('/updatenotes/:id', fetchuser, 
+   async (req, res) => {
+      console.log("note")
+      try {
+         const { title, description, tag } = req.body;
+        
+         //create newNote object
+         const newNote = {};
+         if (title) { newNote.title = title }
+         if (description) { newNote.description = description }
+         if (tag) { newNote.tag = tag }
+        
+
+         //find the note and update it
+         let note = await Note.findById(req.params.id);
+         if (!note) { return res.status(404).send("Not Found") }
+
+         if (note.user.toString() !== req.user.id) {
+            return res.status(404).send("Not Found")
+         }
+
+         note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+
+         res.json({ note })
       }
       catch (error) {
          res.status(500).send("Internal server error");
